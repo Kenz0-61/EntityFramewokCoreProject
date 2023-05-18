16a7f205a5db88813f3a0a7e422ad421ca695840
Kenz0-61 <enes.eou@outlook.com>
2023-05-18 17:45:32 +0300

    Test Deneme Branch18/05/2023

---
+++ EntityFramewokCoreProject/Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EntityFramewokCoreProject.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
